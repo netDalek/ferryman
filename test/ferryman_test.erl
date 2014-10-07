ff5bbd55a58c4d7dc8e9e6e8c1a80d57908f1fc3
d.kirichenko <d.kirichenko@fun-box.ru>
2014-10-07 11:14:51 +0400

    добавил плагин для erlang

---
+++ test/ferryman_test.erl
@@ -0,0 +1,14 @@
+-module(ferryman_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+
+handler(<<"test">>, []) -> ok;
+handler(_, _) -> throw(method_not_found).
+
+rpc_test() ->
+  ferryman_server:start_link("127.0.0.1", 6379, 0, ["a2p"], fun handler/2),
+  {ok, Redis} = eredis:start_link(),
+  {ok, R1} = ferryman_client:call(Redis, "a2p", test, []),
+  ?assertEqual(<<"ok">>, R1),
+  {error, _} = ferryman_client:call(Redis, "a2p", dd, []).
