eed0a1842779662c6b1809c41350885db1e010c4
d.kirichenko <d.kirichenko@fun-box.ru>
2014-10-07 14:54:21 +0400

    Add multicall and timeout

--- spec/ferryman_spec.rb
+++ spec/ferryman_spec.rb
@@ -10,10 +10,13 @@ describe "ferryman" do
     def sum(a, b)
       a + b
     end
+    def long
+      sleep(2)
+    end
   end
 
   it "receive call and cast messages" do
-    client = Ferryman::Client.new(Redis.new, "dd")
+    client = Ferryman::Client.new(Redis.new, "dd", 1)
     server = Ferryman::Server.new(Redis.new, "dd")
     t = Thread.start(server) do |server|
       server.receive(TTT.new)
@@ -23,6 +26,7 @@ describe "ferryman" do
     expect(client.call(:sum, 1, 2)).to eql(3)
     expect(client.multicall(:sum, 1, 2)).to eql([3])
     expect{ client.call(:raise) }.to raise_error(Ferryman::Error)
+    expect{ client.call(:long) }.to raise_error(Timeout::Error)
     t.kill
   end
 end
