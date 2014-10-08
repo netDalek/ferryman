e14319f25662ec4ae8240991bac65a9078b5f5b3
d.kirichenko <d.kirichenko@fun-box.ru>
2014-10-08 17:13:52 +0400

    async rpc processing

--- src/ferryman_server.erl
+++ src/ferryman_server.erl
@@ -1,7 +1,8 @@
 -module(ferryman_server).
 
 -export([
-         start_link/5
+         start_link/5,
+         put_reply/3
         ]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
@@ -14,6 +15,9 @@
 
 start_link(Host, Port, Db, Channels, Handler) ->
   gen_server:start_link(?MODULE, [Host, Port, Db, Channels, Handler], []).
+
+put_reply(Pid, Id, Msg) ->
+  gen_server:call(Pid, {put_reply, Id, Msg}).
 
 %% ===================================================================
 %% GEN SERVER
@@ -30,6 +34,10 @@ init([Host, Port, Db, Channels, Handler]) ->
 handle_cast(_Msg, St) ->
     {stop, error, St}.
 
+handle_call({put_reply, Id, Msg}, _From, St) ->
+    eredis:q(St#st.client, ["RPUSH", Id, Msg]),
+    eredis:q(St#st.client, ["DEL", Id]),
+    {reply, ok, St};
 handle_call(stop, _From, St) ->
     {stop, normal, shutdown_ok, St};
 handle_call(_Msg, _From, St) ->
@@ -37,17 +45,8 @@ handle_call(_Msg, _From, St) ->
 
 handle_info({message, _Channel, Message, _Pid}, St) ->
   error_logger:info_msg("ferryman_server receive message ~s", [Message]),
-  case jsonrpc2:handle(Message, St#st.handler, fun jiffy:decode/1, fun jiffy:encode/1) of
-    noreply ->
-      error_logger:info_msg("ferryman_server don't reply for cast message ~s", [Message]),
-      nop;
-    {reply, Msg} ->
-      {MsgAttrs} = jiffy:decode(Msg),
-      Id = proplists:get_value(<<"id">>, MsgAttrs),
-      error_logger:info_msg("ferryman_server reply for message ~s", [Id]),
-      eredis:q(St#st.client, ["RPUSH", Id, Msg]),
-      eredis:q(St#st.client, ["DEL", Id])
-  end,
+  Self = self(),
+  spawn_link(fun() -> handle_request(Self, Message, St#st.handler) end),
   eredis_sub:ack_message(St#st.sub),
   {noreply, St};
 handle_info(_Info, #st{sub = Sub} = St) ->
@@ -57,3 +56,22 @@ handle_info(_Info, #st{sub = Sub} = St) ->
 code_change(_OldVsn, St, _Extra) -> {ok, St}.
 
 terminate(_Reason, _St) -> ok.
+
+%% ===================================================================
+%% PRIVATE
+%% ===================================================================
+
+handle_request(ParentPid, Message, Handler) ->
+  case jsonrpc2:handle(Message, Handler, fun jiffy:decode/1, fun jiffy:encode/1) of
+    noreply ->
+      error_logger:info_msg("ferryman_server don't reply for cast message ~s", [Message]),
+      nop;
+    {reply, Msg} ->
+      {MsgAttrs} = jiffy:decode(Msg),
+      Id = proplists:get_value(<<"id">>, MsgAttrs),
+      error_logger:info_msg("ferryman_server reply for message ~s", [Id]),
+      put_reply(ParentPid, Id, Msg)
+      % eredis:q(St#st.client, ["RPUSH", Id, Msg]),
+      % eredis:q(St#st.client, ["DEL", Id])
+  end.
+
